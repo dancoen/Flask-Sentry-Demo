8bde738fba587f467ab435a1de45d8f917f473ff
Dan Coen <coen_daniel@bah.com>
2019-11-08 13:58:54 -0500

    added sentry integration

--- FlaskApp.py
+++ FlaskApp.py
@@ -1,11 +1,14 @@
-import flask
-
 from flask import Flask
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_sdk.init(
+    dsn="https://bc07b6a5e8d04bafb2206f26c7177449@sentry.io/1813727",
+    integrations=[FlaskIntegration()]
+)
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
-
-if __name__ == "__main__":
-    app.run()
