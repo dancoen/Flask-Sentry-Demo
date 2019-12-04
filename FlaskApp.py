a817e2d8251b2a1f2ca25e18a89624382ea0e7b2
Dan Coen <coen_daniel@bah.com>
2019-12-04 16:46:34 -0500

    adding error for flask test

--- FlaskApp.py
+++ FlaskApp.py
@@ -1,14 +1,11 @@
 from flask import Flask
 import sentry_sdk
-from sentry_sdk.integrations.flask import FlaskIntegration
-
-sentry_sdk.init(
-    dsn="https://bc07b6a5e8d04bafb2206f26c7177449@sentry.io/1813727",
-    integrations=[FlaskIntegration()]
-)
+sentry_sdk.init("https://bc07b6a5e8d04bafb2206f26c7177449@sentry.io/1813727")
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+division_by_zero = 1 / 0
