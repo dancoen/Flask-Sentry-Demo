fd7137f5e264e7bfc7e727a16afcc313e22b4649
Dan Coen <coen_daniel@bah.com>
2019-12-05 11:15:38 -0500

    Removed exception statement again, created sentry deployment

--- FlaskApp.py
+++ FlaskApp.py
@@ -1,6 +1,11 @@
 from flask import Flask
 import sentry_sdk
-sentry_sdk.init("https://bc07b6a5e8d04bafb2206f26c7177449@sentry.io/1813727")
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_sdk.init(
+    dsn="https://bc07b6a5e8d04bafb2206f26c7177449@sentry.io/1813727",
+    integrations=[FlaskIntegration()]
+)
 
 app = Flask(__name__)
 
@@ -8,4 +13,4 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-division_by_zero = 1 / 0
+#division_by_zero = 1 / 0
